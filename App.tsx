d122c3b72db018c522d2c69576e95b527e9b99e4
gabimmdev <gabrielagomeescezar@gmail.com>
2025-05-21 21:35:26 -0300

    feat: configura rotas condicionais com React Navigation baseado na autenticação

--- App.tsx
+++ App.tsx
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthProvider, useAuth } from "./src/contexts/AuthContext";
 
-export default function App() {
+import { LoginScreen } from "./src/screens/LoginScreen";
+import { RegisterScreen } from "./src/screens/RegisterScreen";
+import { HomeScreen } from "./src/screens/HomeScreen";
+import { MotoDetailsScreen } from "./src/screens/MotoDetailScreen";
+import { ActivityIndicator, View } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+function AppRoutes() {
+  const { token, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator screenOptions={{ headerShown: true }}>
+      {token ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Pátio de Motos" }} />
+          <Stack.Screen name="MotoDetails" component={MotoDetailsScreen} options={{ title: "Detalhes da Moto" }} />
+        </>
+      ) : (
+        <>
+          <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Login" }} />
+          <Stack.Screen name="Register" component={RegisterScreen} options={{ title: "Cadastro" }} />
+        </>
+      )}
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <AuthProvider>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </AuthProvider>
+  );
+}
