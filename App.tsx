5b2031d4fa9ebb697f164fd3d333ac3bea53bef7
gabimmdev <gabrielagomescezar@gmail.com>
2025-05-23 09:46:15 -0300

    fix: deleting unused code

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthProvider } from "./src/contexts/AuthContext";
 import AppNavigator from "./src/navigation/AppNavigator";
 
 export default function App() {
   return (
-    <AuthProvider>
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
-    </AuthProvider>
   );
 }
