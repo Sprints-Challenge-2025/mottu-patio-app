98c3310bec8159bc3f6f063052e6ad20a005ff10
gabimmdev <gabrielagomeescezar@gmail.com>
2025-05-25 17:55:20 -0300

    fix: add authprovider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { AuthProvider } from "./src/contexts/AuthContext";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
+    <AuthProvider>
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
+    </AuthProvider>
   );
 }
