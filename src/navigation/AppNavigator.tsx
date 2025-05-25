ec3ac9839e560e617ae5345162f9998a8b16f1ac
gabimmdev <gabrielagomeescezar@gmail.com>
2025-05-25 17:59:23 -0300

    fix: change inicial route, add route to inicial screen and change order

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -6,24 +6,26 @@ import RegisterScreen from "../screens/RegisterScreen";
 import HomeScreen from "../screens/HomeScreen";
 import MotoDetailsScreen from "../screens/MotoDetailScreen";
 import RegisterMotoScreen from "../screens/RegisterMotoScreen";
+import InicialScreen from "../screens/InicialScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <Stack.Navigator
-      initialRouteName="Login"
+      initialRouteName="Inicial"
       screenOptions={{
         headerTitle: "MottuSense",
         headerTintColor: "#21D445FF",
         headerTitleStyle: { fontWeight: "bold", fontSize: 20 },
       }}
     >
+      <Stack.Screen name="Inicial" component={InicialScreen} options={{ headerShown: false }}/>
       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
-      <Stack.Screen name="MotoDetails" component={MotoDetailsScreen} options={{title: "Detalhes da Moto", headerShown: false }}/>
       <Stack.Screen name="RegisterMoto" component={RegisterMotoScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="MotoDetails" component={MotoDetailsScreen} options={{title: "Detalhes da Moto", headerShown: false }}/>
     </Stack.Navigator>
   );
 }
