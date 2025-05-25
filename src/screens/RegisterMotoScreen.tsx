01ce85d9fdb2a781190913a1524687b99e3c11a4
gabimmdev <gabrielagomeescezar@gmail.com>
2025-05-25 18:02:29 -0300

    feat: add logo, back link and style

--- src/screens/RegisterMotoScreen.tsx
+++ src/screens/RegisterMotoScreen.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Moto } from "../types";
+import { Image } from "react-native";
 
 export default function RegisterMotoScreen({ navigation }: any) {
   const [placa, setPlaca] = useState("");
@@ -36,6 +37,9 @@ export default function RegisterMotoScreen({ navigation }: any) {
 
   return (
     <View style={styles.container}>
+      <View style={styles.topBar}>
+        <Image source={require("../assets/images/logo.png")} style={styles.logo} />
+      </View>
       <Text style={styles.titulo}>Cadastrar Nova Moto</Text>
 
       <TextInput placeholder="Placa" value={placa} onChangeText={setPlaca} style={styles.input} />
@@ -47,13 +51,16 @@ export default function RegisterMotoScreen({ navigation }: any) {
       <TouchableOpacity onPress={handleSalvar} style={styles.botao}>
         <Text style={styles.textoBotao}>Salvar Moto</Text>
       </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.goBack()} >
+        <Text style={styles.linkVoltar}>Voltar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16 },
-  titulo: { fontSize: 20, fontWeight: "bold", marginBottom: 16 },
+  titulo: { fontSize: 20, fontWeight: "bold", marginBottom: 16, marginTop: 90 },
   input: {
     borderWidth: 1,
     borderColor: "#ccc",
@@ -72,5 +79,23 @@ const styles = StyleSheet.create({
     color: "#fff",
     fontWeight: "bold",
     fontSize: 16,
+  },
+  topBar: {
+    marginTop: 50,
+    marginHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logo: {
+    width: 70,
+    height: 70,
+    resizeMode: "contain",
+  },
+  linkVoltar: {
+    marginTop: 16,
+    textAlign: "center",
+    color: "#2DAB26FF",
+    fontSize: 17,
   },
 });
